5ea0182e2c23fff1015ce842125c23918d13008c
RealMVG <kuroshit23@gmail.com>
2025-03-08 19:24:44 +0200

    Added Login Controller, UserModel and View

---
+++ Task_1/Controllers/LoginController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Task_1.Models;
+
+namespace Task_1.Controllers;
+
+public class LoginController : Controller
+{
+    private static List<UserModel> users = new List<UserModel>()
+    {
+        new UserModel { Id = 1, Username = "admin", Password = "admin", IsAdmin = true },
+        new UserModel { Id = 2, Username = "user", Password = "user", IsAdmin = false },
+    };
+        
+    public IActionResult Login()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public ActionResult Login(UserModel model)
+    {
+        var user = users.SingleOrDefault(u => u.Username == model.Username && u.Password == model.Password);
+
+        if (user != null)
+        {
+            return RedirectToAction("Index", "Home");
+        }
+        
+        ViewBag.Message = "Invalid username or password";
+
+        return View();
+    }
+
+}
